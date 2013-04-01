84e4e084c9d1af770b3a38307f52a22a33df23a6
Daniel Manning <daniel.manning@demandmedia.com>
2013-04-01 16:31:16 -0500

    moved windows projects

---
+++ Windows/Program.cs
@@ -0,0 +1,24 @@
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace BulletMLLibQuickStart.Windows
+{
+	static class Program
+	{
+		private static Game1 game;
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main ()
+		{
+			game = new Game1 ();
+			game.Run ();
+		}
+	}
+}
